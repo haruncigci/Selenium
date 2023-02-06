c50b7bff6c0e42d0e1477e8d3b06518ff570315b
Cigci <cigciharun@gmail.com>
2023-02-06 18:12:39 +0100

    asdf

---
+++ src/test/java/video4_WebElements/WebElements02.java
@@ -0,0 +1,16 @@
+package video4_WebElements;
+
+public class WebElement02 {
+    public static void main(String[] args) {
+        // https://www.facebook.com adresine gidin
+        // cookies cikarsa kabul et butonuna basin
+        // e-posta kutusuna rastgele bir mail girin
+        // sifre kismina rastgele bir sifre girin
+        // giris yap butonuna basin
+        // uyari olarak "The email or mobile number you entered isn't connected to an account. Find your"
+        // mesajinin ciktigini test edin
+        // sayfayi kapatin
+        
+
+    }
+}
---
+++ src/test/java/video4_WebElements/WebElements03.java
@@ -0,0 +1,11 @@
+package video4_WebElements;
+
+public class WebElements03 {
+    public static void main(String[] args) {
+        // 1 - https://www.bestbuy.com adresine gidin
+        // 2 - cookies cikarsa kabul et butonuna basin
+        // 3 - sayfada kac adet link bulundugunu yazdirin
+        // 4 - sayfadaki herbir linkin üzerindeki yazilari yazdirin
+
+    }
+}
